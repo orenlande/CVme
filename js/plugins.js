dec5d2f81313abe8c84d084c1b1988cc75f1f122
Oren Lande <oren@lending-express.com>
2019-01-05 23:07:29 +0200

    a

--- js/plugins.js
+++ js/plugins.js
@@ -22,70 +22,84 @@
 }());
 
 // Place any jQuery/helper plugins in here.
-// The Browser API key obtained from the Google API Console.
+ // The Browser API key obtained from the Google API Console.
     // Replace with your own Browser API key, or your own key.
-    // var developerKey = 'AIzaSyC86Z-BbOuO-BGI2CvMmUHO3iR0FB02eXw';
+    var developerKey = 'AIzaSyC86Z-BbOuO-BGI2CvMmUHO3iR0FB02eXw';
 
-    // // The Client ID obtained from the Google API Console. Replace with your own Client ID.
-    // var clientId = "344473574277-i16ved7bkg1mu58focj6igl8p221fcm2.apps.googleusercontent.com"
+    // The Client ID obtained from the Google API Console. Replace with your own Client ID.
+    var clientId = "344473574277-i16ved7bkg1mu58focj6igl8p221fcm2.apps.googleusercontent.com"
 
-    // // Scope to use to access user's photos.
-    //   var scope = [
-    //     'https://www.googleapis.com/auth/drive.readonly',
-    //   ];
-    //   var authApiLoaded = false;
-    //   var pickerApiLoaded = false;
-    //   var oauthToken;
-    //   var viewIdForhandleAuthResult;
-    //   // Use the API Loader script to load google.picker and gapi.auth.
-    //   function onApiLoad() {
-    //     gapi.load('auth', {'callback': onAuthApiLoad});
-    //     gapi.load('picker', {'callback': onPickerApiLoad});
-    //   }
-    //   function onAuthApiLoad() {
-    //     authApiLoaded = true;
-    //   }
-    //   function onPickerApiLoad() {
-    //     pickerApiLoaded = true;
-    //   }
-    //   function handleAuthResult(authResult) {
-    //     if (authResult && !authResult.error) {
-    //       oauthToken = authResult.access_token;
-    //       createPicker(viewIdForhandleAuthResult, true);
-    //     }
-    //   }
-    //   // Create and render a Picker object for picking user Photos.
-    //   function createPicker(viewId, setOAuthToken) {
-    //     if (authApiLoaded && pickerApiLoaded) {
-    //       var picker;
-          
-    //       if(authApiLoaded && oauthToken && setOAuthToken) {
-    //         picker = new google.picker.PickerBuilder().
-    //           addView(viewId).
-    //           setOAuthToken(oauthToken).
-    //           setDeveloperKey(developerKey).
-    //           setCallback(pickerCallback).
-    //           build();
-    //       } else {
-    //         picker = new google.picker.PickerBuilder().
-    //           addView(viewId).
-    //           setDeveloperKey(developerKey).
-    //           setCallback(pickerCallback).
-    //           build();
-    //       }
-          
-    //       picker.setVisible(true);
-    //     }
-    //   }
-    //   // A simple callback implementation.
-    //   function pickerCallback(data) {
-    //     var url = 'nothing';
-    //     if (data[google.picker.Response.ACTION] == google.picker.Action.PICKED) {
-    //       var doc = JSON.stringify(data[google.picker.Response.DOCUMENTS][0], null, "  ");
-    //     }
-    //     var message = 'You picked: <br>' + doc;
-    //     document.getElementById('result').innerHTML = message;
-    //   }
+    // Replace with your own project number from console.developers.google.com.
+    // See "Project number" under "IAM & Admin" > "Settings"
+    var appId = "344473574277";
+
+    // Scope to use to access user's Drive items.
+    var scope = ['https://www.googleapis.com/auth/drive.readonly'];
+
+    var pickerApiLoaded = false;
+    var oauthToken;
+
+    // Use the Google API Loader script to load the google.picker script.
+    function loadPicker() {
+      gapi.load('auth', {'callback': onAuthApiLoad});
+      gapi.load('picker', {'callback': onPickerApiLoad});
+    }
+
+    function onAuthApiLoad() {
+        console.log('auth loaded')
+      window.gapi.auth.authorize(
+          {
+            'client_id': clientId,
+            'scope': scope,
+            'immediate': false
+          },
+          handleAuthResult);
+    }
+
+    function onPickerApiLoad() {
+        console.log('api loaded')
+      pickerApiLoaded = true;
+      createPicker();
+    }
+
+    function handleAuthResult(authResult) {
+      if (authResult && !authResult.error) {
+        oauthToken = authResult.access_token;
+        createPicker();
+      }
+    }
+
+    // Create and render a Picker object for searching images.
+    function createPicker() {
+        console.log(pickerApiLoaded, oauthToken);
+      if (pickerApiLoaded && oauthToken) {
+        var view = new google.picker.View(google.picker.ViewId.DOCS);
+        view.setMimeTypes("application/pdf,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document");
+        var picker = new google.picker.PickerBuilder()
+            .enableFeature(google.picker.Feature.NAV_HIDDEN)
+            .enableFeature(google.picker.Feature.MULTISELECT_ENABLED)
+            .setAppId(appId)
+            .setOAuthToken(oauthToken)
+            .addView(view)
+            .addView(new google.picker.DocsUploadView())
+            .setDeveloperKey(developerKey)
+            .setCallback(pickerCallback)
+            .build();
+          picker.setVisible(true);
+      }
+    }
+
+    // A simple callback implementation.
+    function pickerCallback(data) {
+      if (data.action == google.picker.Action.PICKED) {
+        var fileId = data.docs[0].id;
+        alert('The user selected: ' + fileId);
+      }
+    }
+
+    $("#pick").on("click", function(){
+        loadPicker();
+    }) 
 
 
     
